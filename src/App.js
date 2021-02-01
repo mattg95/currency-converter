f700aab99700a99bc83b52c3137f685b12558237
mattg95 <mattgoodman95@gmail.com@>
2021-02-01 14:51:57 +0000

    working currency converter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const currencyRates = {
+    EUR: 1.13,
+    USD: 1.37,
+    AUD: 1.79,
+  };
+
+  const [state, setState] = useState({
+    currency: "USD",
+    gbpAmount: 0,
+    convertedCurrencyAmount: 0,
+    isConverted: false,
+  });
+  const { currency, gbpAmount, convertedCurrencyAmount, isConverted } = state;
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setState({ ...state, [name]: value, isConverted: false });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const conversionRate = currencyRates[currency];
+    const convertedAmount = gbpAmount * conversionRate;
+    setState({
+      ...state,
+      convertedCurrencyAmount: convertedAmount,
+      isConverted: true,
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Curreny Converter</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="gbpAmount">Enter GBP:</label>
+        <input
+          name="gbpAmount"
+          type="number"
+          onChange={(e) => handleChange(e)}
+        ></input>
+        <select name="currency" onChange={(e) => handleChange(e)}>
+          <option>USD</option>
+          <option>AUD</option>
+          <option>EUR</option>
+        </select>
+        <input type="submit"></input>
+      </form>
+      {isConverted && (
+        <div>
+          Your amount is: {convertedCurrencyAmount} {currency}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
